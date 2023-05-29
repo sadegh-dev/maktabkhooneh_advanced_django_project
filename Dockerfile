be9e0857eb281a6dc026b3185746f4aa56c7ac9e
sadegh-dev <sadegh.rez@gmail.com>
2023-05-29 19:45:18 +0330

    set environments in python-decouple'

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /app
 
 COPY requirements.txt /app/
 
-RUN pip install --upgrade pip 
-RUN pip install -r requirements.txt
+RUN pip3 install --upgrade pip 
+RUN pip3 install -r requirements.txt
 
 COPY ./core /app/
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)1s^tv0qzyq6^lm5zt$bc*ff&q5v1!v*uzoyj%nsr#4)sm9+ae'
+#SECRET_KEY = 'django-insecure-)1s^tv0qzyq6^lm5zt$bc*ff&q5v1!v*uzoyj%nsr#4)sm9+ae'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
