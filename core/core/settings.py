7c3ca117f632f9d894ff28b677bda886dd13a057
sadegh-dev <sadegh.rez@gmail.com>
2023-07-17 17:04:51 +0330

    #1 install python-decouple package #2 set config keys:values of python-decouple in settings file

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zycxl)x2izh11t%brm^b6hpu=!*%098(j0t1sr8&sekpbil@4e'
+#SECRET_KEY = 'django-insecure-zycxl)x2izh11t%brm^b6hpu=!*%098(j0t1sr8&sekpbil@4e'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast= lambda v : [s.strip() for s in v.split(',')], default=[])
 
 # Application definition
 
