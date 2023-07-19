8de8c07e5815a0e15197f94be1474c80b9c00015
sadegh-dev <sadegh.rez@gmail.com>
2023-07-19 10:04:53 +0330

    definition Category-class and Post-class in blog app

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+    author = models.ForeignKey(
+        'User', 
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True
+    )
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
