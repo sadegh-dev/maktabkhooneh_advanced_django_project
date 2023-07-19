ff3a1ccbe1159240cb67ed67d79f42197da9a6f2
sadegh-dev <sadegh.rez@gmail.com>
2023-07-19 11:00:05 +0330

    create User-class in accounts app

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,32 +1,38 @@
-from django.db import models
+# from django.db import models
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=250)
+# class Category(models.Model):
+#     """
+#     class of Category include jus name field
+#     """
+#     name = models.CharField(max_length=250)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
-class Post(models.Model):
-    image = models.ImageField(null=True, blank=True)
-    title = models.CharField(max_length=250)
-    content = models.TextField()
-    status = models.BooleanField(default=False)
-    published_date = models.DateTimeField()
-    author = models.ForeignKey(
-        'User', 
-        on_delete=models.CASCADE
-    )
-    category = models.ForeignKey(
-        Category, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        blank=True
-    )
+# class Post(models.Model):
+#     """
+#     class of Book has two ForeignKey relations.
+#     """
+#     image = models.ImageField(null=True, blank=True)
+#     title = models.CharField(max_length=250)
+#     content = models.TextField()
+#     status = models.BooleanField(default=False)
+#     published_date = models.DateTimeField()
+#     author = models.ForeignKey(
+#         'User', 
+#         on_delete=models.CASCADE
+#     )
+#     category = models.ForeignKey(
+#         Category, 
+#         on_delete=models.SET_NULL, 
+#         null=True, 
+#         blank=True
+#     )
 
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+#     created_date = models.DateTimeField(auto_now_add=True)
+#     updated_date = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
