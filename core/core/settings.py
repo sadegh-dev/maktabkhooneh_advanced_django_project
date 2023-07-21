2a00cdbca7ea1534982dadf9a4113f795c7d00e9
sadegh-dev <sadegh.rez@gmail.com>
2023-07-21 09:24:39 +0330

    #set environments in docker-compose [SECRET_KEY and DEBUG]

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zycxl)x2izh11t%brm^b6hpu=!*%098(j0t1sr8&sekpbil@4e'
-#SECRET_KEY = config("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-zycxl)x2izh11t%brm^b6hpu=!*%098(j0t1sr8&sekpbil@4e'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = config("DEBUG", cast=bool)
+#DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 #ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast= lambda v : [s.strip() for s in v.split(',')], default=[])
