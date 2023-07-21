2303f7a3b5fd1d8e8fc88b7818c53e24eccb8c4e
sadegh-dev <sadegh.rez@gmail.com>
2023-07-21 15:09:10 +0330

    # create admin-panel class for User

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    # form for change information
+    fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_active', 'is_staff', 'is_superuser'
+            ),
+        }),
+    )
+
+    # form for add information
+    add_fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'email', 'password1', 'password2' 
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_active', 'is_staff', 'is_superuser'
+            ),
+        }),
+    )
+
+
+
+
+
+admin.site.register(User, CustomUserAdmin)
+
+
