58fe4bc33955e01d504a73f61f9e427895edb95f
sadegh-dev <sadegh.rez@gmail.com>
2023-07-22 20:53:46 +0330

    create Profile-class

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -53,6 +53,8 @@ class CustomUserManager(BaseUserManager):
 * AbstractBaseUser and AbstractUser has password field by default.
 """
 
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """
     about class User :
@@ -78,4 +80,16 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField(max_length=5000)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.email} | {self.first_name} {self.last_name}"
 
