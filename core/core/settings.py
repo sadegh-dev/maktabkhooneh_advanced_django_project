e22186ed2151e53c17b5ae3e28addbc2f09fa043
sadegh-dev <sadegh.rez@gmail.com>
2023-07-22 21:52:32 +0330

    set media and static settings'

--- core/core/settings.py
+++ core/core/settings.py
@@ -122,6 +122,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# more static directories
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiels",
+    #"/var/www/static/",
+]
+
+
+# Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
