9be9bd035614fc87b63cda6add3002c8dc6b7f14
sadegh-dev <sadegh.rez@gmail.com>
2023-08-06 16:42:32 +0330

    touch urls.py in blog app and config in urls main in core

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog'))
 ]
 
 
@@ -30,7 +31,6 @@ urlpatterns = [
     Use in Development status 
     for media and static files 
 '''
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
