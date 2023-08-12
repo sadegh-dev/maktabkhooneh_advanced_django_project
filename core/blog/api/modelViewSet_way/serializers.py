d0b19e35e8746d8ba28d0ac812fa5007d3d8b7d4
sadegh-dev <sadegh.rez@gmail.com>
2023-08-12 17:52:17 +0330

    blog app > coding CategorySerializer and PostSerializer

---
+++ core/blog/api/modelViewSet_way/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from blog.models import Category, Post
+
+
+class CategorySerializer(serializer.ModelSerializer):
+    class Meta :
+        fields = ['id', 'name']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    ''' class Serializer for Post-model '''
+    class Meta :
+        fields = ['id', 'image', 'title', 'content',
+                   'status', 'published_date', 'author', 
+                   'category', 'created_date', 'updated_date']
+
+
