46d1bedd5a8fd55e1ce08f3898a9da8e83c9f948
sadegh-dev <sadegh.rez@gmail.com>
2023-08-12 18:11:25 +0330

    blog app > api > fix error - edit serializer-classes

--- core/blog/api/modelViewSet_way/serializers.py
+++ core/blog/api/modelViewSet_way/serializers.py
@@ -4,12 +4,14 @@ from blog.models import Category, Post
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta :
+        model = Category
         fields = ['id', 'name']
 
 
 class PostSerializer(serializers.ModelSerializer):
     ''' class Serializer for Post-model '''
     class Meta :
+        model = Post
         fields = ['id', 'image', 'title', 'content',
                    'status', 'published_date', 'author', 
                    'category', 'created_date', 'updated_date']
