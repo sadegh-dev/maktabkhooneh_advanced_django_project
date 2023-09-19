e069c03aa46b3c6c4d1ed70e657b0d5ba8871b13
sadegh-dev <sadegh.rez@gmail.com>
2023-09-19 17:31:26 +0330

    refactor Post model class

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -5,6 +5,14 @@ class Post(models.Model):
     This is to save details of blog posts.
     
     '''
+    author = models.ForeignKey('User', 
+                               on_delete=models.CASCADE )
+    
+    category = models.ForeignKey('Category', 
+                                 on_delete=models.SET_NULL, 
+                                 null=True, 
+                                 blank=True )
+    
     title = models.CharField(max_length=250)
     content = models.TextField()
     image = models.ImageField(null=True, blank=True)
@@ -13,14 +21,6 @@ class Post(models.Model):
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-
-    author = models.ForeignKey('User', 
-                               on_delete=models.CASCADE )
-    
-    category = models.ForeignKey('Category', 
-                                 on_delete=models.SET_NULL, 
-                                 null=True, 
-                                 blank=True )
     
     def __str__(self):
         return self.title
