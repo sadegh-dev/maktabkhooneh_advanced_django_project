8f46c5d6df22f5ca053e7cd73feffec33427b098
sadegh-dev <sadegh.rez@gmail.com>
2023-09-19 16:39:40 +0330

    Definition of Post model class fields

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -5,3 +5,24 @@ class Post(models.Model):
     This is to save details of blog posts.
     
     '''
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey('User', 
+                               on_delete=models.CASCADE )
+    
+    category = models.ForeignKey('Category', 
+                                 on_delete=models.SET_NULL, 
+                                 null=True, 
+                                 blank=True )
+    
+    def __str__(self):
+        return self.title
+    
+    
