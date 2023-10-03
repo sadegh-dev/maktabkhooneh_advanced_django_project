f550dbff99b9d1565f5f2fe5797294f41829d292
sadegh-dev <sadegh.rez@gmail.com>
2023-10-03 23:39:37 +0330

    develop models of blog app

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    '''
+    A model class to determine the category of posts.
+    '''
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(models.Model):
+    '''
+    This is to save details of blog posts.
+    
+    '''
+    author = models.ForeignKey('User', 
+                               on_delete=models.CASCADE )
+    
+    category = models.ForeignKey('Category', 
+                                 on_delete=models.SET_NULL, 
+                                 null=True, 
+                                 blank=True )
+    
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
